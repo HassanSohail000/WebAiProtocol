6b2943b182827983e095a8d9d1394f1902e098c6
Hassan Sohail <107843990+HassanSohail000@users.noreply.github.com>
2025-01-15 16:53:45 +0500

    push

--- Assets/Scripts/Console.cs
+++ Assets/Scripts/Console.cs
@@ -5,27 +5,48 @@ using TMPro;
 public class Console : MonoBehaviour
 {
     public TMP_Text consoleText;
+    public Button clearButton;
+    public ScrollRect scrollRect;
     private Queue<string> logQueue = new Queue<string>();
     private const int maxLogCount = 20;
 
     void OnEnable()
     {
         Application.logMessageReceived += HandleLog;
+        clearButton.onClick.AddListener(ClearLog);
     }
 
     void OnDisable()
     {
         Application.logMessageReceived -= HandleLog;
+        clearButton.onClick.RemoveListener(ClearLog);
     }
 
     void HandleLog(string logString, string stackTrace, LogType type)
     {
-        logQueue.Enqueue(logString);
-        if (logQueue.Count > maxLogCount)
+        if (type == LogType.Error || type == LogType.Exception)
         {
-            logQueue.Dequeue();
-        }
+            string detailedLog = $"[{type}] {logString}\n{stackTrace}\nScript: {GetScriptName()}\nObject: {gameObject.name}";
+            logQueue.Enqueue(detailedLog);
+            if (logQueue.Count > maxLogCount)
+            {
+                logQueue.Dequeue();
+            }
 
-        consoleText.text = string.Join("\n", logQueue.ToArray());
+            consoleText.text = string.Join("\n", logQueue.ToArray());
+            Canvas.ForceUpdateCanvases();
+            scrollRect.verticalNormalizedPosition = 0;
+        }
+    }
+
+    void ClearLog()
+    {
+        logQueue.Clear();
+        consoleText.text = string.Empty;
+    }
+
+    string GetScriptName()
+    {
+        return GetType().Name;
     }
 }
